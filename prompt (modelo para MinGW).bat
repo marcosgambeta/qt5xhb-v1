90be9672c50303d070bd8f2e53a22d0b7916ac4d
Marcos Antonio Gambeta <marcosgambeta@uol.com.br>
2017-01-30 02:30:20 -0300

    adding files for MinGW

---
+++ prompt (modelo para MinGW).bat
@@ -0,0 +1,41 @@
+rem
+rem Qt5xHb - bibliotecas de ligação entre Harbour/xHarbour e Qt Framework 5
+rem
+rem Copyright (C) 2012-2017 Marcos Antonio Gambeta <marcosgambeta@uol.com.br>
+rem
+rem Prompt de comando para utilização do Qt5xHb com MinGW.
+rem
+
+rem Cria um PATH reduzido. Embora opcional, evita conflitos com outras
+rem ferramentas de programação instaladas no ambiente de desenvolvimento.
+set PATH=%SYSTEMROOT%;%SYSTEMROOT%\system32
+
+rem Pasta da instalação do Qt Framework.
+set QTDIR=C:\Qt\Qt5.4.0\5.4\mingw491_32
+
+rem Pasta da instalação do Harbour.
+set HBDIR=C:\Harbour
+
+rem Pasta da instalação do compilador C/C++.
+set CPPDIR=C:\Qt\Qt5.4.0\Tools\mingw491_32
+
+rem Configura a variável PATH, conforme as variáveis definidas acima.
+set PATH=%QTDIR%\bin;%HBDIR%\bin;%CPPDIR%\bin;%PATH%
+
+rem Pasta include do Qt5xHb.
+set QT5XHB_INC_DIR=C:\qt5xhb\include
+
+rem Pasta lib do Qt5xHb.
+rem Exemplo: C:\qt5xhb\lib\qt540\opengl\mingw\32\windows\harbour\320dev
+set QT5XHB_LIB_DIR=
+
+%ComSpec%
+
+rem Limpa as variáveis criadas.
+set QTDIR=
+set HBDIR=
+set CPPDIR=
+set QT5XHB_INC_DIR=
+set QT5XHB_LIB_DIR=
+
+pause
