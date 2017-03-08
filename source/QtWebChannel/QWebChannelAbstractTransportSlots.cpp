173287d435fcb6a6b527b1dcb26267406c1d2702
Marcos Antonio Gambeta <marcosgambeta@uol.com.br>
2017-03-08 08:15:26 -0300

    QtWebChannel module changed to use the function 'Signals_connection_disconnection'

--- source/QtWebChannel/QWebChannelAbstractTransportSlots.cpp
+++ source/QtWebChannel/QWebChannelAbstractTransportSlots.cpp
@@ -43,60 +43,9 @@ HB_FUNC( QWEBCHANNELABSTRACTTRANSPORT_ONMESSAGERECEIVED )
   {
     s = new SlotsQWebChannelAbstractTransport(QCoreApplication::instance());
   }
-  bool ret = false;
-  if( hb_pcount() == 1 )
-  {
-    QObject* object = (QObject*) hb_itemGetPtr( hb_objSendMsg( hb_stackSelfItem(), "POINTER", 0 ) );
-    QString signal = "messageReceived(QJsonObject,QWebChannelAbstractTransport*";
-    bool connected = Signals_is_signal_connected( object, signal );
-    if( !connected )
-    {
-      PHB_ITEM codeblock = hb_itemNew( hb_param( 1, HB_IT_BLOCK | HB_IT_SYMBOL ) );
-      if( codeblock )
-      {
-        ret = object->connect( object, SIGNAL(messageReceived(QJsonObject,QWebChannelAbstractTransport*), s, SLOT(messageReceived(QJsonObject,QWebChannelAbstractTransport*) );
-        if( ret )
-        {
-          Signals_connect_signal( object, signal, codeblock ); // se conectado, adiciona
-          hb_retl(ret);
-        }
-        else
-        {
-          hb_itemRelease( codeblock );
-          hb_retl(ret);
-        }
-      }
-      else
-      {
-        hb_retl(false);
-      }
-    }
-    else
-    {
-      hb_retl(false);
-    }
-  }
-  else if( hb_pcount() == 0 )
-  {
-    QObject* object = (QObject*) hb_itemGetPtr( hb_objSendMsg( hb_stackSelfItem(), "POINTER", 0 ) );
-    QString signal = "messageReceived(QJsonObject,QWebChannelAbstractTransport*";
-    ret = object->disconnect(object, SIGNAL(messageReceived(QJsonObject,QWebChannelAbstractTransport*), s, SLOT(messageReceived(QJsonObject,QWebChannelAbstractTransport*) );
-    if( ret )
-    {
-      Signals_disconnect_signal( object, signal );
-      hb_retl(true);
-    }
-    else
-    {
-      hb_retl(false);
-    }
-  }
-  else
-  {
-    hb_retl(false);
-  }
+
+  hb_retl( Signals_connection_disconnection ( s, "messageReceived(QJsonObject,QWebChannelAbstractTransport*)", "messageReceived(QJsonObject,QWebChannelAbstractTransport*)" ) );
 #else
   hb_retl(false);
 #endif
 }
-
--- source/QtWebChannel/QWebChannelSlots.cpp
+++ source/QtWebChannel/QWebChannelSlots.cpp
@@ -42,60 +42,9 @@ HB_FUNC( QWEBCHANNEL_OBBLOCKUPDATESCHANGED )
   {
     s = new SlotsQWebChannel(QCoreApplication::instance());
   }
-  bool ret = false;
-  if( hb_pcount() == 1 )
-  {
-    QObject* object = (QObject*) hb_itemGetPtr( hb_objSendMsg( hb_stackSelfItem(), "POINTER", 0 ) );
-    QString signal = "blockUpdatesChanged(bool)";
-    bool connected = Signals_is_signal_connected( object, signal );
-    if( !connected )
-    {
-      PHB_ITEM codeblock = hb_itemNew( hb_param( 1, HB_IT_BLOCK | HB_IT_SYMBOL ) );
-      if( codeblock )
-      {
-        ret = object->connect( object, SIGNAL(blockUpdatesChanged(bool)), s, SLOT(blockUpdatesChanged(bool)) );
-        if( ret )
-        {
-          Signals_connect_signal( object, signal, codeblock ); // se conectado, adiciona
-          hb_retl(ret);
-        }
-        else
-        {
-          hb_itemRelease( codeblock );
-          hb_retl(ret);
-        }
-      }
-      else
-      {
-        hb_retl(false);
-      }
-    }
-    else
-    {
-      hb_retl(false);
-    }
-  }
-  else if( hb_pcount() == 0 )
-  {
-    QObject* object = (QObject*) hb_itemGetPtr( hb_objSendMsg( hb_stackSelfItem(), "POINTER", 0 ) );
-    QString signal = "blockUpdatesChanged(bool)";
-    ret = object->disconnect(object, SIGNAL(blockUpdatesChanged(bool)), s, SLOT(blockUpdatesChanged(bool)) );
-    if( ret )
-    {
-      Signals_disconnect_signal( object, signal );
-      hb_retl(true);
-    }
-    else
-    {
-      hb_retl(false);
-    }
-  }
-  else
-  {
-    hb_retl(false);
-  }
+
+  hb_retl( Signals_connection_disconnection ( s, "blockUpdatesChanged(bool)", "blockUpdatesChanged(bool)" ) );
 #else
   hb_retl(false);
 #endif
 }
-
