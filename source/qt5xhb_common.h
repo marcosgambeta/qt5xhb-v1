5b320aa6f48a78eedb55340124bee01293531fdd
Marcos Antonio Gambeta <marcosgambeta@uol.com.br>
2017-03-11 12:30:02 -0300

    added new file qt5xhb_common.h

---
+++ source/qt5xhb_common.h
@@ -0,0 +1,24 @@
+/*
+
+  Qt5xHb - biblioteca de ligação entre Harbour/xHarbour e Qt Framework 5
+
+  Copyright (C) 2012-2017 Marcos Antonio Gambeta <marcosgambeta@uol.com.br>
+
+*/
+
+#include "hbapi.h"
+#include "hbapiitm.h"
+#include "hbapierr.h"
+#include "hbvm.h"
+#include "hbstack.h"
+
+#ifndef __XHARBOUR__
+#include "hbapicls.h"
+#define ISNIL HB_ISNIL
+#define ISLOG HB_ISLOG
+#define ISNUM HB_ISNUM
+#define ISCHAR HB_ISCHAR
+#define ISPOINTER HB_ISPOINTER
+#define ISOBJECT HB_ISOBJECT
+#define ISARRAY HB_ISARRAY
+#endif
