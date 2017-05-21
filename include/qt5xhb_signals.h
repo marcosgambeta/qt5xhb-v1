6533e61a1db4ff657b246c093f45ddddfc3b0150
Marcos Antonio Gambeta <marcosgambeta@uol.com.br>
2017-05-21 03:59:48 -0300

    Added file qt5xhb_signals.h
    Added:
    include\qt5xhb_signals.h

---
+++ include/qt5xhb_signals.h
@@ -0,0 +1,15 @@
+/*
+
+  Qt5xHb - bibliotecas de ligação entre Harbour/xHarbour e Qt Framework 5
+
+  Copyright (C) 2012-2017 Marcos Antonio Gambeta <marcosgambeta@uol.com.br>
+
+*/
+
+bool Signals_connect_signal ( QObject * object, QString signal, PHB_ITEM codeblock );
+bool Signals_disconnect_signal ( QObject * object, QString signal );
+bool Signals_is_signal_connected( QObject * object, QString signal );
+PHB_ITEM Signals_return_codeblock ( QObject * object, QString signal );
+void Signals_release_codeblocks ();
+void Signals_disconnect_all_signals (QObject * obj);
+bool Signals_connection_disconnection ( QObject * s, QString signal, QString slot );
