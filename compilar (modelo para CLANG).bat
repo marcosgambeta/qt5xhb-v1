a7df7f224beee9cd07586c719cc3af866225bbcc
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2017-10-28 22:11:23 -0200

    Added batch files for CLANG/MSYS
    Added:
    compilar (modelo para CLANG).bat
    prompt (modelo para CLANG).bat

---
+++ compilar (modelo para CLANG).bat
@@ -0,0 +1,86 @@
+rem
+rem Qt5xHb - bibliotecas de ligação entre Harbour/xHarbour e Qt Framework 5
+rem
+rem Copyright (C) 2017 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+rem
+rem Batch para compilação (Windows/CLANG/MSYS)
+rem
+
+rem
+rem Utilize este .bat como modelo para compilar as bibliotecas para Windows
+rem usando CLANG/MSYS, fazendo uma cópia e ajustando os parâmetros e caminhos.
+rem
+
+rem Cria um PATH reduzido. Embora opcional, evita conflitos com outras
+rem ferramentas de programação instaladas no ambiente de desenvolvimento.
+set PATH=%SYSTEMROOT%;%SYSTEMROOT%\system32
+
+rem Pasta da instalação do Qt Framework (use mingw32 ou mingw64).
+set QTDIR=C:\msys64\mingw32
+
+rem Pasta da instalação do Harbour.
+set HBDIR=C:\harbour
+
+rem Pasta da instalação do compilador C/C++.
+set CPPDIR=%QTDIR%
+
+rem Configura a variável PATH, conforme as variáveis definidas acima.
+set PATH=%QTDIR%\bin;%HBDIR%\bin;%CPPDIR%\bin;%PATH%
+
+rem Define se é Harbour ou xHarbour (harbour ou xharbour)
+set HBCOMP=harbour
+
+rem Define a versão do Harbour/xHarbour (320dev, 340dev, ...)
+set HBVER=320dev
+
+rem Define a versão do Qt (qt540, qt541, ...)
+set QTVERSION=qt592
+
+rem Define se é 32-bit ou 64-bit (32 ou 64)
+set QT32OR64=32
+
+rem Define se é Angle, OpenGL ou Dynamic (angle, opengl ou dynamic)
+set QTANGLEOROPENGL=dynamic
+
+rem Define o compilador C++ (clang ou clang64)
+set QTCOMP=clang
+
+rem Define a plataforma (windows)
+set QTPLATFORM=windows
+
+rem Cria a pasta para as bibliotecas, caso não exista.
+if not exist lib mkdir lib
+if not exist lib\%QTVERSION% mkdir lib\%QTVERSION%
+if not exist lib\%QTVERSION%\%QTANGLEOROPENGL% mkdir lib\%QTVERSION%\%QTANGLEOROPENGL%
+if not exist lib\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP% mkdir lib\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%
+if not exist lib\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64% mkdir lib\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64%
+if not exist lib\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64%\%QTPLATFORM% mkdir lib\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64%\%QTPLATFORM%
+if not exist lib\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64%\%QTPLATFORM%\%HBCOMP% mkdir lib\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64%\%QTPLATFORM%\%HBCOMP%
+if not exist lib\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64%\%QTPLATFORM%\%HBCOMP%\%HBVER% mkdir lib\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64%\%QTPLATFORM%\%HBCOMP%\%HBVER%
+
+rem Cria a pasta para os objetos, caso não exista.
+if not exist obj mkdir obj
+if not exist obj\%QTVERSION% mkdir obj\%QTVERSION%
+if not exist obj\%QTVERSION%\%QTANGLEOROPENGL% mkdir obj\%QTVERSION%\%QTANGLEOROPENGL%
+if not exist obj\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP% mkdir obj\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%
+if not exist obj\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64% mkdir obj\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64%
+if not exist obj\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64%\%QTPLATFORM% mkdir obj\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64%\%QTPLATFORM%
+if not exist obj\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64%\%QTPLATFORM%\%HBCOMP% mkdir obj\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64%\%QTPLATFORM%\%HBCOMP%
+if not exist obj\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64%\%QTPLATFORM%\%HBCOMP%\%HBVER% mkdir obj\%QTVERSION%\%QTANGLEOROPENGL%\%QTCOMP%\%QT32OR64%\%QTPLATFORM%\%HBCOMP%\%HBVER%
+
+rem Compila as bibliotecas.
+mingw32-make -f makefile_clang 1>%QTVERSION%_%QTANGLEOROPENGL%_%QTCOMP%_%QT32OR64%_%QTPLATFORM%_%HBCOMP%_%HBVER%-1.log 2>%QTVERSION%_%QTANGLEOROPENGL%_%QTCOMP%_%QT32OR64%_%QTPLATFORM%_%HBCOMP%_%HBVER%-2.log
+
+rem Limpa as variáveis criadas.
+set QTDIR=
+set HBDIR=
+set CPPDIR=
+set HBCOMP=
+set HBVER=
+set QTVERSION=
+set QT32OR64=
+set QTANGLEOROPENGL=
+set QTCOMP=
+set QTPLATFORM=
+
+pause
---
+++ prompt (modelo para CLANG).bat
@@ -0,0 +1,45 @@
+rem
+rem Qt5xHb - bibliotecas de ligação entre Harbour/xHarbour e Qt Framework 5
+rem
+rem Copyright (C) 2017 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+rem
+rem Prompt de comando para utilização do Qt5xHb com CLANG/MSYS.
+rem
+
+rem Cria um PATH reduzido. Embora opcional, evita conflitos com outras
+rem ferramentas de programação instaladas no ambiente de desenvolvimento.
+set PATH=%SYSTEMROOT%;%SYSTEMROOT%\system32
+
+rem Pasta da instalação do Qt Framework (use mingw32 ou mingw64).
+set QTDIR=C:\msys64\mingw32
+
+rem Pasta da instalação do Harbour.
+set HBDIR=C:\harbour
+
+rem Pasta da instalação do compilador C/C++.
+set CPPDIR=%QTDIR%
+
+rem Configura a variável PATH, conforme as variáveis definidas acima.
+set PATH=%QTDIR%\bin;%HBDIR%\bin;%CPPDIR%\bin;%PATH%
+
+rem Pasta include do Qt5xHb.
+set QT5XHB_INC_DIR=include
+
+rem Pasta lib do Qt5xHb.
+rem Exemplo: C:\qt5xhb\lib\qt540\opengl\mingw\32\windows\harbour\320dev
+set QT5XHB_LIB_DIR=
+
+rem Define o compilador C++ (clang ou clang64)
+set HB_COMPILER=clang
+
+%ComSpec%
+
+rem Limpa as variáveis criadas.
+set QTDIR=
+set HBDIR=
+set CPPDIR=
+set QT5XHB_INC_DIR=
+set QT5XHB_LIB_DIR=
+set HB_COMPILER=
+
+pause
