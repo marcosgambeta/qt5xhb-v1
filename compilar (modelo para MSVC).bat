8bfb5db7b1761e07b3d57b28c140566418731506
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2017-11-20 09:07:52 -0200

    *.bat: updates (copyright and email)
    Modified:
    compilar (modelo para MinGW).bat
    compilar (modelo para MSVC).bat
    prompt (modelo para MinGW).bat
    prompt (modelo para MSVC).bat

--- compilar (modelo para MSVC).bat
+++ compilar (modelo para MSVC).bat
@@ -1,7 +1,7 @@
 rem
 rem Qt5xHb - bibliotecas de ligação entre Harbour/xHarbour e Qt Framework 5
 rem
-rem Copyright (C) 2012-2017 Marcos Antonio Gambeta <marcosgambeta@uol.com.br>
+rem Copyright (C) 2017 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
 rem
 rem Batch para compilação (Windows/MSVC)
 rem
