2f90d223ebef36f1d22442ce13f3ba7fa53cf09a
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2018-01-01 12:56:20 -0200

    tests/*.*: updated
    Modified:
    tests/ColorsNames1.prg
    tests/ColorsNames2.prg
    tests/ColorsNames3.prg
    tests/compile.bat
    tests/compileall.bat
    tests/copydlls.bat
    tests/deldlls.bat
    tests/deltests.bat
    tests/HAbstractListModel1.prg
    tests/HAbstractListModel2.prg
    tests/HAbstractListModel3.prg
    tests/HAbstractTableModel1.prg
    tests/HAbstractTableModel10.prg
    tests/HAbstractTableModel11.prg
    tests/HAbstractTableModel2.prg
    tests/HAbstractTableModel3.prg
    tests/HAbstractTableModel4.prg
    tests/HAbstractTableModel5.prg
    tests/HAbstractTableModel6.prg
    tests/HAbstractTableModel7.prg
    tests/HAbstractTableModel8.prg
    tests/HAbstractTableModel9.prg
    tests/HAbstractTableModelV21.prg
    tests/HAbstractTableModelV210.prg
    tests/HAbstractTableModelV211.prg
    tests/HAbstractTableModelV212.prg
    tests/HAbstractTableModelV213.prg
    tests/HAbstractTableModelV22.prg
    tests/HAbstractTableModelV23.prg
    tests/HAbstractTableModelV24.prg
    tests/HAbstractTableModelV25.prg
    tests/HAbstractTableModelV26.prg
    tests/HAbstractTableModelV27.prg
    tests/HAbstractTableModelV28.prg
    tests/HAbstractTableModelV29.prg
    tests/HCodeBlockValidator1.prg
    tests/HCodeBlockValidator2.prg
    tests/QBoxLayout1.prg
    tests/QBoxLayout2.prg
    tests/QBoxLayout3.prg
    tests/QBoxLayout4.prg
    tests/QButtonGroup.prg
    tests/QCalendarWidget.prg
    tests/QCheckBox1.prg
    tests/QCheckBox2.prg
    tests/QCheckBox3.prg
    tests/QColorDialog1.prg
    tests/QColorDialog2.prg
    tests/QComboBox.prg
    tests/QCommandLinkButton1.prg
    tests/QCommandLinkButton2.prg
    tests/QDateEdit.prg
    tests/QDateTimeEdit.prg
    tests/QDesktopWidget.prg
    tests/QDial1.prg
    tests/QDial2.prg
    tests/QDialog.prg
    tests/QDockWidget.prg
    tests/QDoubleSpinBox.prg
    tests/QErrorMessage.prg
    tests/QFileDialog1.prg
    tests/QFileDialog2.prg
    tests/QFocusFrame.prg
    tests/QFontComboBox.prg
    tests/QFontDialog.prg
    tests/QFormLayout1.prg
    tests/QFormLayout2.prg
    tests/QFormLayout3.prg
    tests/QGraphicsEffect.prg
    tests/QGridLayout.prg
    tests/QGroupBox.prg
    tests/QHBoxLayout.prg
    tests/QInputDialog.prg
    tests/QLabel1.prg
    tests/QLabel2.prg
    tests/QLabel3.prg
    tests/QLCDNumber.prg
    tests/QListView.prg
    tests/QMainWindow.prg
    tests/QMdiArea.prg
    tests/QMenu1.prg
    tests/QMenu2.prg
    tests/QMessageBox1.prg
    tests/QMessageBox2.prg
    tests/QMovie.prg
    tests/QPlainTextEdit.prg
    tests/QPrintPreviewDialog.prg
    tests/QProgressBar.prg
    tests/QProgressDialog.prg
    tests/QPushButton1.prg
    tests/QPushButton2.prg
    tests/QPushButton3.prg
    tests/QPushButton4.prg
    tests/QPushButton5.prg
    tests/QPushButton6.prg
    tests/QQuickView.prg
    tests/QQuickWidget.prg
    tests/QRadioButton1.prg
    tests/QRadioButton2.prg
    tests/QRubberBand.prg
    tests/QScrollBar.prg
    tests/QShortcut.prg
    tests/QSlider.prg
    tests/QSpinBox.prg
    tests/QStandardPaths.prg
    tests/QStatusBar.prg
    tests/QTableWidget1.prg
    tests/QTableWidget2.prg
    tests/QTableWidget3.prg
    tests/QTimeEdit.prg
    tests/QTimer1.prg
    tests/QTimer2.prg
    tests/QTimer3.prg
    tests/QToolBox1.prg
    tests/QToolBox2.prg
    tests/QToolButton1.prg
    tests/QToolButton2.prg
    tests/QToolButton3.prg
    tests/QToolButton4.prg
    tests/QTreeWidget.prg
    tests/QtSqlDemo.prg
    tests/QUiLoader.prg
    tests/QVBoxLayout.prg
    tests/QWebView.prg
    tests/QWidget.prg
    tests/QWizard.prg
    tests/setfont.prg
    tests/testpdf.prg
    tests/version.prg

--- tests/compile.bat
+++ tests/compile.bat
@@ -1,7 +1,7 @@
 rem
 rem Qt5xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 5
 rem
-rem Copyright (C) 2017 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+rem Copyright (C) 2018 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
 rem
 rem Batch to compile a test
 rem
