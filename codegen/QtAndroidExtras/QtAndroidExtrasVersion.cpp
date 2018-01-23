da8f8521f6052e9b2d868c242bf6e989a19e7ccb
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2018-01-22 22:19:45 -0200

    QtAndroidExtras: added data for code generation (.cpp and .h files)
    Added:
    codegen/QtAndroidExtras/QtAndroidExtrasVersion.cpp

---
+++ codegen/QtAndroidExtras/QtAndroidExtrasVersion.cpp
@@ -0,0 +1,43 @@
+/*
+
+  Qt5xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 5
+
+  Copyright (C) 2018 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+
+*/
+
+#include <Qt>
+
+#ifndef __XHARBOUR__
+#if (QT_VERSION >= QT_VERSION_CHECK(5,2,0))
+#include <QtAndroidExtrasVersion>
+#endif
+#endif
+
+#include "qt5xhb_common.h"
+#include "qt5xhb_macros.h"
+#include "qt5xhb_utils.h"
+
+#ifdef __XHARBOUR__
+#if (QT_VERSION >= QT_VERSION_CHECK(5,2,0))
+#include <QtAndroidExtrasVersion>
+#endif
+#endif
+
+HB_FUNC( QTANDROIDEXTRAS_VERSION_STR )
+{
+#if (QT_VERSION >= QT_VERSION_CHECK(5,2,0))
+  hb_retc( (const char *) QTANDROIDEXTRAS_VERSION_STR );
+#else
+  hb_retc( (const char *) "" );
+#endif
+}
+
+HB_FUNC( QTANDROIDEXTRAS_VERSION )
+{
+#if (QT_VERSION >= QT_VERSION_CHECK(5,2,0))
+  hb_retni( QTANDROIDEXTRAS_VERSION );
+#else
+  hb_retni( 0 );
+#endif
+}
