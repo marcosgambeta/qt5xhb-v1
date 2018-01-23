64d6abd1d9fa21aeec902ec1d2e6cdaaeff2093b
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2018-01-23 04:40:53 -0200

    QtDeclarative: data for code generation updated
    Modified:
    codegen/QtDeclarative/QDeclarativeComponentSlots.cpp
    codegen/QtDeclarative/QDeclarativeComponentSlots.h
    codegen/QtDeclarative/QDeclarativeEngineSlots.cpp
    codegen/QtDeclarative/QDeclarativeEngineSlots.h
    codegen/QtDeclarative/QDeclarativeExpressionSlots.cpp
    codegen/QtDeclarative/QDeclarativeExpressionSlots.h
    codegen/QtDeclarative/QDeclarativePropertyMapSlots.cpp
    codegen/QtDeclarative/QDeclarativePropertyMapSlots.h
    codegen/QtDeclarative/QDeclarativeViewSlots.cpp
    codegen/QtDeclarative/QDeclarativeViewSlots.h
    codegen/QtDeclarative/QtDeclarativeVersion.cpp

--- codegen/QtDeclarative/QDeclarativeViewSlots.h
+++ codegen/QtDeclarative/QDeclarativeViewSlots.h
@@ -1,23 +1,12 @@
-/*
+%%
+%% Qt5xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 5
+%%
+%% Copyright (C) 2018 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+%%
 
-  Qt5xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 5
+$header
 
-  Copyright (C) 2018 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
-
-*/
-
-#ifndef SLOTSQDECLARATIVEVIEW_H
-#define SLOTSQDECLARATIVEVIEW_H
-
-#include <QObject>
-#include <QCoreApplication>
-#include <QString>
-
-#include <QDeclarativeView>
-
-#include "qt5xhb_common.h"
-#include "qt5xhb_macros.h"
-#include "qt5xhb_signals.h"
+$includes
 
 class SlotsQDeclarativeView: public QObject
 {
@@ -29,5 +18,3 @@ class SlotsQDeclarativeView: public QObject
   void sceneResized ( QSize size );
   void statusChanged ( QDeclarativeView::Status status );
 };
-
-#endif // SLOTSQDECLARATIVEVIEW_H
