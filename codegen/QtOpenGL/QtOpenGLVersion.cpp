928c3de1c5a735c8b27e4c187d0f4f1d18fe28dc
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2018-01-23 19:53:34 -0200

    QtOpenGL: added data for code generation (.cpp and .h files)
    Added:
    codegen/QtOpenGL/QtOpenGLVersion.cpp

---
+++ codegen/QtOpenGL/QtOpenGLVersion.cpp
@@ -0,0 +1,31 @@
+/*
+
+  Qt5xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 5
+
+  Copyright (C) 2018 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+
+*/
+
+#include <Qt>
+
+#ifndef __XHARBOUR__
+#include <QtOpenGLVersion>
+#endif
+
+#include "qt5xhb_common.h"
+#include "qt5xhb_macros.h"
+#include "qt5xhb_utils.h"
+
+#ifdef __XHARBOUR__
+#include <QtOpenGLVersion>
+#endif
+
+HB_FUNC( QTOPENGL_VERSION_STR )
+{
+  hb_retc( (const char *) QTOPENGL_VERSION_STR );
+}
+
+HB_FUNC( QTOPENGL_VERSION )
+{
+  hb_retni( QTOPENGL_VERSION );
+}
