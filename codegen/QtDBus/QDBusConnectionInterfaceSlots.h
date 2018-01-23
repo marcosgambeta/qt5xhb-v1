484b66998570799e55c9352407d5355d4f9565d0
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2018-01-23 04:21:32 -0200

    QtDBus: data for code generation updated
    Modified:
    codegen/QtDBus/QDBusConnectionInterfaceSlots.cpp
    codegen/QtDBus/QDBusConnectionInterfaceSlots.h
    codegen/QtDBus/QDBusPendingCallWatcherSlots.cpp
    codegen/QtDBus/QDBusPendingCallWatcherSlots.h
    codegen/QtDBus/QDBusServerSlots.cpp
    codegen/QtDBus/QDBusServerSlots.h
    codegen/QtDBus/QDBusServiceWatcherSlots.cpp
    codegen/QtDBus/QDBusServiceWatcherSlots.h
    codegen/QtDBus/QtDBusVersion.cpp

--- codegen/QtDBus/QDBusConnectionInterfaceSlots.h
+++ codegen/QtDBus/QDBusConnectionInterfaceSlots.h
@@ -1,23 +1,12 @@
-/*
+%%
+%% Qt5xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 5
+%%
+%% Copyright (C) 2018 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+%%
 
-  Qt5xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 5
+$header
 
-  Copyright (C) 2018 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
-
-*/
-
-#ifndef SLOTSQDBUSCONNECTIONINTERFACE_H
-#define SLOTSQDBUSCONNECTIONINTERFACE_H
-
-#include <QObject>
-#include <QCoreApplication>
-#include <QString>
-
-#include <QDBusConnectionInterface>
-
-#include "qt5xhb_common.h"
-#include "qt5xhb_macros.h"
-#include "qt5xhb_signals.h"
+$includes
 
 class SlotsQDBusConnectionInterface: public QObject
 {
@@ -31,5 +20,3 @@ class SlotsQDBusConnectionInterface: public QObject
   void serviceOwnerChanged(const QString &name, const QString &oldOwner, const QString &newOwner);
   void callWithCallbackFailed(const QDBusError &error, const QDBusMessage &call);
 };
-
-#endif // SLOTSQDBUSCONNECTIONINTERFACE_H
--- codegen/QtDBus/QDBusServiceWatcherSlots.h
+++ codegen/QtDBus/QDBusServiceWatcherSlots.h
@@ -1,23 +1,12 @@
-/*
+%%
+%% Qt5xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 5
+%%
+%% Copyright (C) 2018 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+%%
 
-  Qt5xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 5
+$header
 
-  Copyright (C) 2018 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
-
-*/
-
-#ifndef SLOTSQDBUSSERVICEWATCHER_H
-#define SLOTSQDBUSSERVICEWATCHER_H
-
-#include <QObject>
-#include <QCoreApplication>
-#include <QString>
-
-#include <QDBusServiceWatcher>
-
-#include "qt5xhb_common.h"
-#include "qt5xhb_macros.h"
-#include "qt5xhb_signals.h"
+$includes
 
 class SlotsQDBusServiceWatcher: public QObject
 {
@@ -30,5 +19,3 @@ class SlotsQDBusServiceWatcher: public QObject
   void serviceUnregistered(const QString &service);
   void serviceOwnerChanged(const QString &service, const QString &oldOwner, const QString &newOwner);
 };
-
-#endif // SLOTSQDBUSSERVICEWATCHER_H
