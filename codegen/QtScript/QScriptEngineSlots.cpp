8f339c69698ac068b47cb176b45ee921fab6353a
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2018-01-23 23:41:00 -0200

    QtScript: data for code generation added (.cpp and .h files)
    Added:
    codegen/QtScript/QScriptEngineSlots.cpp
    codegen/QtScript/QScriptEngineSlots.h
    codegen/QtScript/QtScriptVersion.cpp

---
+++ codegen/QtScript/QScriptEngineSlots.cpp
@@ -0,0 +1,44 @@
+/*
+
+  Qt5xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 5
+
+  Copyright (C) 2018 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+
+*/
+
+#include "QScriptEngineSlots.h"
+
+
+static SlotsQScriptEngine * s = NULL;
+
+SlotsQScriptEngine::SlotsQScriptEngine(QObject *parent) : QObject(parent)
+{
+}
+
+SlotsQScriptEngine::~SlotsQScriptEngine()
+{
+}
+
+void SlotsQScriptEngine::signalHandlerException(const QScriptValue & exception)
+{
+  QObject *object = qobject_cast<QObject *>(sender());
+  PHB_ITEM cb = Signals_return_codeblock( object, "signalHandlerException(QScriptValue)" );
+  if( cb )
+  {
+    PHB_ITEM psender = hb_itemPutPtr( NULL, (QObject *) object );
+    PHB_ITEM pexception = hb_itemPutPtr( NULL, (QScriptValue *) &exception );
+    hb_vmEvalBlockV( (PHB_ITEM) cb, 2, psender, pexception );
+    hb_itemRelease( psender );
+    hb_itemRelease( pexception );
+  }
+}
+
+HB_FUNC( QSCRIPTENGINE_ONSIGNALHANDLEREXCEPTION )
+{
+  if( s == NULL )
+  {
+    s = new SlotsQScriptEngine(QCoreApplication::instance());
+  }
+
+  hb_retl( Signals_connection_disconnection ( s, "signalHandlerException(QScriptValue)", "signalHandlerException(QScriptValue)" ) );
+}
