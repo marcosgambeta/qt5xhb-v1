e2914b1c5baac3d007a438771500ca566744c7a7
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2018-01-24 00:00:21 -0200

    QtX11Extras: data for code generation added (.cpp and .h files)
    Added:
    codegen/QtX11Extras/QtX11ExtrasVersion.cpp

---
+++ codegen/QtX11Extras/QtX11ExtrasVersion.cpp
@@ -0,0 +1,43 @@
+/*
+
+  Qt5xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 5
+
+  Copyright (C) 2018 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+
+*/
+
+#include <Qt>
+
+#ifndef __XHARBOUR__
+#if (QT_VERSION >= QT_VERSION_CHECK(5,1,0))
+#include <QtX11ExtrasVersion>
+#endif
+#endif
+
+#include "qt5xhb_common.h"
+#include "qt5xhb_macros.h"
+#include "qt5xhb_utils.h"
+
+#ifdef __XHARBOUR__
+#if (QT_VERSION >= QT_VERSION_CHECK(5,1,0))
+#include <QtX11ExtrasVersion>
+#endif
+#endif
+
+HB_FUNC( QTX11EXTRAS_VERSION_STR )
+{
+#if (QT_VERSION >= QT_VERSION_CHECK(5,1,0))
+  hb_retc( (const char *) QTX11EXTRAS_VERSION_STR );
+#else
+  hb_retc( (const char *) "" );
+#endif
+}
+
+HB_FUNC( QTX11EXTRAS_VERSION )
+{
+#if (QT_VERSION >= QT_VERSION_CHECK(5,1,0))
+  hb_retni( QTX11EXTRAS_VERSION );
+#else
+  hb_retni( 0 );
+#endif
+}
