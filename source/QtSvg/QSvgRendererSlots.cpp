b11a66989f059d6e21ef8a00815ca7e8070661ac
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2020-02-26 16:17:56 -0300

    source/QtSvg: updated
    Modified:
    source/QtSvg/QSvgRendererSlots.cpp

--- source/QtSvg/QSvgRendererSlots.cpp
+++ source/QtSvg/QSvgRendererSlots.cpp
@@ -26,7 +26,7 @@ void QSvgRendererSlots::repaintNeeded()
   if( cb )
   {
     PHB_ITEM psender = Signals_return_qobject ( (QObject *) object, "QSVGRENDERER" );
-    hb_vmEvalBlockV( (PHB_ITEM) cb, 1, psender );
+    hb_vmEvalBlockV( cb, 1, psender );
     hb_itemRelease( psender );
   }
 }
