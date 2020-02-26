0808569c5bce83a53023e4bc7421dc3300440710
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2020-02-26 16:00:44 -0300

    source/QtScriptTools: updated
    Modified:
    source/QtScriptTools/QScriptEngineDebuggerSlots.cpp

--- source/QtScriptTools/QScriptEngineDebuggerSlots.cpp
+++ source/QtScriptTools/QScriptEngineDebuggerSlots.cpp
@@ -26,7 +26,7 @@ void QScriptEngineDebuggerSlots::evaluationResumed()
   if( cb )
   {
     PHB_ITEM psender = Signals_return_qobject ( (QObject *) object, "QSCRIPTENGINEDEBUGGER" );
-    hb_vmEvalBlockV( (PHB_ITEM) cb, 1, psender );
+    hb_vmEvalBlockV( cb, 1, psender );
     hb_itemRelease( psender );
   }
 }
@@ -37,7 +37,7 @@ void QScriptEngineDebuggerSlots::evaluationSuspended()
   if( cb )
   {
     PHB_ITEM psender = Signals_return_qobject ( (QObject *) object, "QSCRIPTENGINEDEBUGGER" );
-    hb_vmEvalBlockV( (PHB_ITEM) cb, 1, psender );
+    hb_vmEvalBlockV( cb, 1, psender );
     hb_itemRelease( psender );
   }
 }
