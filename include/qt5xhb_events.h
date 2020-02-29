de4cd39de932c06edaa0e048a27a03dd5aa5e632
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2020-02-29 17:37:24 -0300

    include/qt5xhb_events.h: new file

---
+++ include/qt5xhb_events.h
@@ -0,0 +1,19 @@
+/*
+
+  Qt5xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 5
+
+  Copyright (C) 2020 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+
+*/
+
+#ifndef QT5XHB_EVENTS_H
+#define QT5XHB_EVENTS_H
+
+bool Events_connect_event ( QObject * object, int type, PHB_ITEM codeblock );
+bool Events_disconnect_event ( QObject * object, int type );
+void Events_release_codeblocks ();
+void Events_disconnect_all_events (QObject * obj, bool children);
+PHB_ITEM Events_return_object ( QEvent * ptr, const char * classname );
+PHB_ITEM Events_return_qobject ( QObject * ptr, const char * classname );
+
+#endif /* QT5XHB_EVENTS_H */
