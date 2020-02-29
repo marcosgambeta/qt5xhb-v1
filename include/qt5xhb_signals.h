44953511d5204e047f7b999c058ff040a56907ec
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2020-02-29 17:38:31 -0300

    include/qt5xhb_signals.h: updated

--- include/qt5xhb_signals.h
+++ include/qt5xhb_signals.h
@@ -11,10 +11,11 @@
 
 bool Signals_connect_signal ( QObject * object, QString signal, PHB_ITEM codeblock );
 bool Signals_disconnect_signal ( QObject * object, QString signal );
-bool Signals_is_signal_connected( QObject * object, QString signal );
+bool Signals_is_signal_connected ( QObject * object, QString signal );
 PHB_ITEM Signals_return_codeblock ( QObject * object, QString signal );
 void Signals_release_codeblocks ();
-void Signals_disconnect_all_signals (QObject * obj);
+//void Signals_disconnect_all_signals (QObject * obj);
+void Signals_disconnect_all_signals (QObject * obj, bool children);
 bool Signals_connection_disconnection ( QObject * s, QString signal, QString slot );
 PHB_ITEM Signals_return_object ( void * ptr, const char * classname );
 PHB_ITEM Signals_return_qobject ( QObject * ptr, const char * classname );
