7df5a557b00d0a36368938f57a6a3db47eb3a741
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2020-05-22 23:17:56 -0300

    include/qt5xhb_signals.h: updated

--- include/qt5xhb_signals.h
+++ include/qt5xhb_signals.h
@@ -9,15 +9,29 @@
 #ifndef QT5XHB_SIGNALS_H
 #define QT5XHB_SIGNALS_H
 
-bool Signals_connect_signal ( QObject * object, QString signal, PHB_ITEM codeblock );
-bool Signals_disconnect_signal ( QObject * object, QString signal );
-bool Signals_is_signal_connected ( QObject * object, QString signal );
-PHB_ITEM Signals_return_codeblock ( QObject * object, QString signal );
-void Signals_release_codeblocks ();
+namespace Qt5xHb
+{
+  bool Signals_connect_signal( QObject * object, QString signal, PHB_ITEM codeblock );
+  bool Signals_disconnect_signal( QObject * object, QString signal );
+  bool Signals_is_signal_connected( QObject * object, QString signal );
+  PHB_ITEM Signals_return_codeblock( QObject * object, QString signal );
+  void Signals_release_codeblocks();
+  //void Signals_disconnect_all_signals (QObject * obj);
+  void Signals_disconnect_all_signals( QObject * obj, bool children );
+  bool Signals_connection_disconnection( QObject * s, QString signal, QString slot );
+  PHB_ITEM Signals_return_object( void * ptr, const char * classname );
+  PHB_ITEM Signals_return_qobject( QObject * ptr, const char * classname );
+}
+
+bool Signals_connect_signal( QObject * object, QString signal, PHB_ITEM codeblock );
+bool Signals_disconnect_signal( QObject * object, QString signal );
+bool Signals_is_signal_connected( QObject * object, QString signal );
+PHB_ITEM Signals_return_codeblock( QObject * object, QString signal );
+void Signals_release_codeblocks();
 //void Signals_disconnect_all_signals (QObject * obj);
-void Signals_disconnect_all_signals (QObject * obj, bool children);
-bool Signals_connection_disconnection ( QObject * s, QString signal, QString slot );
-PHB_ITEM Signals_return_object ( void * ptr, const char * classname );
-PHB_ITEM Signals_return_qobject ( QObject * ptr, const char * classname );
+void Signals_disconnect_all_signals( QObject * obj, bool children );
+bool Signals_connection_disconnection( QObject * s, QString signal, QString slot );
+PHB_ITEM Signals_return_object( void * ptr, const char * classname );
+PHB_ITEM Signals_return_qobject( QObject * ptr, const char * classname );
 
 #endif /* QT5XHB_SIGNALS_H */
