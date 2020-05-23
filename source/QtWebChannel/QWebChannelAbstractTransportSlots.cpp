e9e4dde00a161f9302143d23b42fc42688dbeb04
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2020-05-23 04:31:28 -0300

    source/QtWebChannel: updated
    Modified:
    source/QtWebChannel/QQmlWebChannel.prg
    source/QtWebChannel/QWebChannel.prg
    source/QtWebChannel/QWebChannelAbstractTransport.prg
    source/QtWebChannel/QWebChannelAbstractTransportSlots.cpp
    source/QtWebChannel/QWebChannelSlots.cpp

--- source/QtWebChannel/QWebChannelAbstractTransportSlots.cpp
+++ source/QtWebChannel/QWebChannelAbstractTransportSlots.cpp
@@ -25,11 +25,11 @@ void QWebChannelAbstractTransportSlots::messageReceived( const QJsonObject & mes
 {
   QObject *object = qobject_cast<QObject *>(sender());
 
-  PHB_ITEM cb = Signals_return_codeblock( object, "messageReceived(QJsonObject,QWebChannelAbstractTransport*)" );
+  PHB_ITEM cb = Qt5xHb::Signals_return_codeblock( object, "messageReceived(QJsonObject,QWebChannelAbstractTransport*)" );
 
   if( cb )
   {
-    PHB_ITEM psender = Signals_return_qobject( (QObject *) object, "QWEBCHANNELABSTRACTTRANSPORT" );
+    PHB_ITEM psender = Qt5xHb::Signals_return_qobject( (QObject *) object, "QWEBCHANNELABSTRACTTRANSPORT" );
     PHB_ITEM pmessage = Signals_return_object( (void *) &message, "QJSONOBJECT" );
     PHB_ITEM ptransport = Signals_return_qobject( (QObject *) transport, "QWEBCHANNELABSTRACTTRANSPORT" );
 
@@ -58,7 +58,7 @@ void QWebChannelAbstractTransportSlots_connect_signal( const QString & signal, c
       s->setParent( QCoreApplication::instance() );
     }
 
-    hb_retl( Signals_connection_disconnection( s, signal, slot ) );
+    hb_retl( Qt5xHb::Signals_connection_disconnection( s, signal, slot ) );
   }
   else
   {
--- source/QtWebChannel/QWebChannelSlots.cpp
+++ source/QtWebChannel/QWebChannelSlots.cpp
@@ -25,11 +25,11 @@ void QWebChannelSlots::blockUpdatesChanged( bool block )
 {
   QObject *object = qobject_cast<QObject *>(sender());
 
-  PHB_ITEM cb = Signals_return_codeblock( object, "blockUpdatesChanged(bool)" );
+  PHB_ITEM cb = Qt5xHb::Signals_return_codeblock( object, "blockUpdatesChanged(bool)" );
 
   if( cb )
   {
-    PHB_ITEM psender = Signals_return_qobject( (QObject *) object, "QWEBCHANNEL" );
+    PHB_ITEM psender = Qt5xHb::Signals_return_qobject( (QObject *) object, "QWEBCHANNEL" );
     PHB_ITEM pblock = hb_itemPutL( NULL, block );
 
     hb_vmEvalBlockV( cb, 2, psender, pblock );
@@ -56,7 +56,7 @@ void QWebChannelSlots_connect_signal( const QString & signal, const QString & sl
       s->setParent( QCoreApplication::instance() );
     }
 
-    hb_retl( Signals_connection_disconnection( s, signal, slot ) );
+    hb_retl( Qt5xHb::Signals_connection_disconnection( s, signal, slot ) );
   }
   else
   {
