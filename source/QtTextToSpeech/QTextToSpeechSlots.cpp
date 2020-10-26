44d91533dc0aa1bccfac5cba2abb94a4967f765f
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2020-10-26 17:58:11 -0300

    source/QtTextToSpeech/QTextToSpeechSlots.cpp: updated

--- source/QtTextToSpeech/QTextToSpeechSlots.cpp
+++ source/QtTextToSpeech/QTextToSpeechSlots.cpp
@@ -30,7 +30,7 @@ void QTextToSpeechSlots::localeChanged( const QLocale & locale )
   if( cb )
   {
     PHB_ITEM psender = Qt5xHb::Signals_return_qobject( (QObject *) object, "QTEXTTOSPEECH" );
-    PHB_ITEM plocale = Signals_return_object( (void *) &locale, "QLOCALE" );
+    PHB_ITEM plocale = Qt5xHb::Signals_return_object( (void *) &locale, "QLOCALE" );
 
     hb_vmEvalBlockV( cb, 2, psender, plocale );
 
@@ -110,7 +110,7 @@ void QTextToSpeechSlots::voiceChanged( const QVoice & voice )
   if( cb )
   {
     PHB_ITEM psender = Qt5xHb::Signals_return_qobject( (QObject *) object, "QTEXTTOSPEECH" );
-    PHB_ITEM pvoice = Signals_return_object( (void *) &voice, "QVOICE" );
+    PHB_ITEM pvoice = Qt5xHb::Signals_return_object( (void *) &voice, "QVOICE" );
 
     hb_vmEvalBlockV( cb, 2, psender, pvoice );
 
