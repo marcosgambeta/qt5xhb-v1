6c46ea77a273b5df020f8e789d8b775f5a1df830
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2021-04-07 08:20:30 -0300

    Update qt5xhb_common.h

--- include/qt5xhb_common.h
+++ include/qt5xhb_common.h
@@ -15,13 +15,6 @@
 
 #ifndef __XHARBOUR__
 #include "hbapicls.h"
-#define ISNIL HB_ISNIL
-#define ISLOG HB_ISLOG
-#define ISNUM HB_ISNUM
-#define ISCHAR HB_ISCHAR
-#define ISPOINTER HB_ISPOINTER
-#define ISOBJECT HB_ISOBJECT
-#define ISARRAY HB_ISARRAY
 #endif
 
 // TOCHECK: for xHarbour compatibility
