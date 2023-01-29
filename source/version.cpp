c3f628acf8d17ecc631c8797f7f51a4bf9331684
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2023-01-29 06:49:10 -0300

    Update version.cpp

--- source/version.cpp
+++ source/version.cpp
@@ -6,24 +6,23 @@
 
 */
 
-//#include <QtCore/QtGlobal>
-#include <QtGlobal>
+#include <QtCore/QtGlobal>
 
 #include <hbapi.h>
 
 HB_FUNC( QT5XHB_VERSION )
 {
-  hb_retc( (const char *) "1.0.0dev" );
+  hb_retc(static_cast<const char*>("1.0.0dev"));
 }
 
 HB_FUNC( QT5XHB_VERSION_DATE )
 {
-  hb_retc( (const char *) "01/01/2023 02:45:00" );
+  hb_retc(static_cast<const char*>("01/01/2023 02:45:00"));
 }
 
 HB_FUNC( QT5XHB_ISCPP11 )
 {
-  hb_retl( 0 );
+  hb_retl(false);
 }
 
 /*
@@ -32,12 +31,12 @@ HB_FUNC( QT5XHB_ISCPP11 )
 
 HB_FUNC( QT4XHB_VERSION )
 {
-  hb_retc( (const char *) "1.0.0dev" );
+  hb_retc(static_cast<const char*>("1.0.0dev"));
 }
 
 HB_FUNC( QT4XHB_VERSION_DATE )
 {
-  hb_retc( (const char *) "01/01/2023 02:45:00" );
+  hb_retc(static_cast<const char*>("01/01/2023 02:45:00"));
 }
 
 /*
@@ -46,25 +45,25 @@ HB_FUNC( QT4XHB_VERSION_DATE )
 
 HB_FUNC( QVERSION )
 {
-  hb_retc( (const char *) qVersion() );
+  hb_retc(static_cast<const char*>(qVersion()));
 }
 
 HB_FUNC( QSHAREDBUILD )
 {
-  hb_retl( qSharedBuild() );
+  hb_retl(qSharedBuild());
 }
 
 HB_FUNC( QT_VERSION_STR )
 {
-  hb_retc( (const char *) QT_VERSION_STR );
+  hb_retc(static_cast<const char*>(QT_VERSION_STR));
 }
 
 HB_FUNC( QT_VERSION )
 {
-  hb_retni( QT_VERSION );
+  hb_retni(QT_VERSION);
 }
 
 HB_FUNC( QT_VERSION_CHECK )
 {
-  hb_retni( QT_VERSION_CHECK( hb_parni(1), hb_parni(2), hb_parni(3) ) );
+  hb_retni(QT_VERSION_CHECK(hb_parni(1), hb_parni(2), hb_parni(3)));
 }
