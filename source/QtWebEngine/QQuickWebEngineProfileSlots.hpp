9f4e2b955b371c6b861f8e9ed6b75e056de945de
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2023-02-03 10:25:54 -0300

    Update QtWebEngine

--- source/QtWebEngine/QQuickWebEngineProfileSlots.hpp
+++ source/QtWebEngine/QQuickWebEngineProfileSlots.hpp
@@ -19,10 +19,10 @@
 
 #include <QtWebEngine/QQuickWebEngineProfile>
 
-#include "qt5xhb_common.h"
-#include "qt5xhb_macros.h"
-#include "qt5xhb_utils.h"
-#include "qt5xhb_signals.h"
+#include "qt5xhb_common.hpp"
+#include "qt5xhb_macros.hpp"
+#include "qt5xhb_utils.hpp"
+#include "qt5xhb_signals.hpp"
 
 class QQuickWebEngineProfileSlots: public QObject
 {
