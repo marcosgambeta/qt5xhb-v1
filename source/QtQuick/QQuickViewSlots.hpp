83ac6e95b10d0fc4d582ba24c22c9a2348e5fc8e
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2023-02-03 10:19:53 -0300

    Update QtQuick

--- source/QtQuick/QQuickViewSlots.hpp
+++ source/QtQuick/QQuickViewSlots.hpp
@@ -19,10 +19,10 @@
 
 #include <QtQuick/QQuickView>
 
-#include "qt5xhb_common.h"
-#include "qt5xhb_macros.h"
-#include "qt5xhb_utils.h"
-#include "qt5xhb_signals.h"
+#include "qt5xhb_common.hpp"
+#include "qt5xhb_macros.hpp"
+#include "qt5xhb_utils.hpp"
+#include "qt5xhb_signals.hpp"
 
 class QQuickViewSlots: public QObject
 {
