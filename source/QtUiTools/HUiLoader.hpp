42741544e7e963410e561c11619de03cc32b152e
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2023-02-03 10:24:28 -0300

    Update QtUiTools

--- source/QtUiTools/HUiLoader.hpp
+++ source/QtUiTools/HUiLoader.hpp
@@ -13,9 +13,9 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QLayout>
 
-#include "qt5xhb_common.h"
-#include "qt5xhb_macros.h"
-#include "qt5xhb_utils.h"
+#include "qt5xhb_common.hpp"
+#include "qt5xhb_macros.hpp"
+#include "qt5xhb_utils.hpp"
 
 class HUiLoader : public QUiLoader
 {
