e1634621a0fb1d44883e2fbec13e44db37b65038
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-10-19 19:12:47 -0300

    Update qt5xhb_macros.hpp: add macro ISQWIDGETORNIL

--- include/qt5xhb_macros.hpp
+++ include/qt5xhb_macros.hpp
@@ -193,5 +193,6 @@
 
 #define ISNUMORNIL(n) (HB_ISNUM(n) || HB_ISNIL(n))
 #define ISCHARORNIL(n) (HB_ISCHAR(n) || HB_ISNIL(n))
+#define ISQWIDGETORNIL(n) (ISQWIDGET(n) || HB_ISNIL(n))
 
 #endif // QT5XHB_MACROS_H
