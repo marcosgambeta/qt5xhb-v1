b2332232cfef6a22a23dccc1155e4b405b51e75b
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-10-28 09:07:24 -0300

    Update qt5xhb_macros.hpp: add macro ISLOGORNIL

--- include/qt5xhb_macros.hpp
+++ include/qt5xhb_macros.hpp
@@ -193,5 +193,6 @@
 
 #define ISNUMORNIL(n) (HB_ISNUM(n) || HB_ISNIL(n))
 #define ISCHARORNIL(n) (HB_ISCHAR(n) || HB_ISNIL(n))
+#define ISLOGORNIL(n) (HB_ISLOG(n) || HB_ISNIL(n))
 
 #endif // QT5XHB_MACROS_H
